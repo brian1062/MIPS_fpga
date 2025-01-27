c34d7dda0ecb43bce1cf1ecd3fb40a67794071b1
Ema-Rodriguez <emanuel.rodriguez@mi.unc.edu.ar>
2025-01-27 20:03:37 -0300

    cambios en test bench pipeline

--- test/tb_pipeline.v
+++ test/tb_pipeline.v
@@ -13,9 +13,7 @@ module tb_pipeline;
     reg                   i_dunit_reset_pc;
     reg                   i_dunit_w_en;
     reg                   i_dunit_r_data;
-    reg [NB_WIDHT-1:0]    i_dunit_addr_data;
-    reg [NB_ADDR-1:0]     i_dunit_addr;
-    reg [NB_REG-1:0]      i_dunit_mem_addr;
+    reg [NB_WIDHT-1:0]    i_dunit_addr;
     reg [NB_REG-1:0]      i_dunit_data_if;
     wire [NB_REG-1:0]     o_dunit_mem_data;
     wire [NB_REG-1:0]     o_dunit_reg;
@@ -39,7 +37,7 @@ module tb_pipeline;
         .i_dunit_clk_en(i_dunit_clk_en),
         .i_dunit_reset_pc(i_dunit_reset_pc),
         .i_dunit_w_mem(i_dunit_w_en),
-        .i_dunit_addr(i_dunit_addr_data),
+        .i_dunit_addr(i_dunit_addr),
         .i_dunit_data_if(i_dunit_data_if),
         .o_dunit_reg(o_dunit_reg),
         .o_dunit_mem_data(o_dunit_mem_data)
@@ -53,9 +51,7 @@ module tb_pipeline;
         i_dunit_reset_pc = 0;
         i_dunit_w_en     = 0;
         i_dunit_r_data   = 0;
-        i_dunit_addr_data = 0;
-        i_dunit_addr      = 0;
-        i_dunit_mem_addr  = 0;
+        i_dunit_addr     = 0;
         i_dunit_data_if   = 0;
 
         // Wait for a few cycles
@@ -66,48 +62,51 @@ module tb_pipeline;
         i_dunit_w_en=1;
         // ----------- Operaciones -----------
 
-        // Instructions
-        // 1. ADDI: Guardar 1 en $1
-        i_dunit_mem_addr = 32'h0000_0004; 
+        // 1. ADDI: Guardar 1 en $1 de la memoria de registros
+        i_dunit_addr = 32'h0000_0004; 
         i_dunit_data_if = 32'b001000_00000_00001_00000_00000_000001; // ADDI $1, $0, 1
+        // Acción esperada: $1 = 1
         #10;
 
-        // 2. SW: Guardar el valor de $1 en la posición 4 de memoria
-        i_dunit_mem_addr = 32'h0000_0008; 
-        i_dunit_data_if = 32'b101011_00000_00001_00000_00000_000100; // SW $1, 4($0)
+        // 2. ADDI: Guardar 2 en $2 de la memoria de registros
+        i_dunit_addr = 32'h0000_0008;
+        i_dunit_data_if = 32'b001000_00000_00010_00000_00000_000010; // ADDI $2, $0, 2
+        // Acción esperada: $2 = 2
         #10;
 
-        // 3. LW: Cargar el valor de la posición 4 de memoria en $2
-        i_dunit_mem_addr = 32'h0000_000C; 
-        i_dunit_data_if = 32'b100011_00000_00010_00000_00000_000100; // LW $2, 4($0)
+        // SW: Guardar el contenido de $2 (valor: 2) en la posición 4 de la memoria de datos
+        i_dunit_addr = 32'h0000_0012;
+        i_dunit_data_if = 32'b101011_00000_00010_00000_00000_000100; // SW $2, 4($0)
+        // Acción esperada: El valor 2 de $2 se almacena en la dirección 4 de la memoria de datos.
         #10;
 
-        // 4. ADDU: Sumar $1 y $2 y guardar el resultado en $3
-        i_dunit_mem_addr = 32'h0000_0010; 
-        i_dunit_data_if = 32'b000000_00001_00010_00011_00000_100001; // ADDU $3, $1, $2
+        // LW: Cargar el contenido de la dirección 4 de la memoria de datos en $17
+        i_dunit_addr = 32'h0000_0016;
+        i_dunit_data_if = 32'b100011_00000_10001_00000_00000_000100; // LW $17, 4($0)
+        // Resultado esperado: $17 = 2 (cargado desde la dirección 4 de la memoria de datos)
         #10;
 
-        // 5. SW: Guardar el valor de $3 en la posición 8 de memoria
-        i_dunit_mem_addr = 32'h0000_0014; 
-        i_dunit_data_if = 32'b101011_00000_00011_00000_00000_001000; // SW $3, 8($0)
+        // SW: Guardar el contenido de $1 (valor: 1) en la posición 8 de la memoria de datos
+        i_dunit_addr = 32'h0000_0020;
+        i_dunit_data_if = 32'b101011_00000_00001_00000_00000_001000; // SW $1, 8($0)
+        // Acción esperada: El valor 1 de $1 se almacena en la dirección 8 de la memoria de datos.
         #10;
 
-
-        // 6. SUBU: Restar $1 y $2 y guardar el resultado en $4
-        i_dunit_mem_addr = 32'h0000_0018; 
-        i_dunit_data_if = 32'b000000_00001_00010_00100_00000_100011; // SUBU $4, $1, $2
-        #10;
-
-        // 7. SW: Guardar el valor de $4 en la posición 12 de memoria
-        i_dunit_mem_addr = 32'h0000_001C; 
-        i_dunit_data_if = 32'b101011_00100_00000_00000_00000_001100; // SW $4, 12($0)
-        #10;
+        // LW: Cargar el contenido de la dirección 8 de la memoria de datos en $18
+        i_dunit_addr = 32'h0000_0024;
+        i_dunit_data_if = 32'b100011_00000_10010_00000_00000_001000; // LW $18, 8($0)
+        // Resultado esperado: $18 = 1 (cargado desde la dirección 8 de la memoria de datos)
 
         i_dunit_w_en = 0;
         i_dunit_reset_pc = 0;
         #10;
         //comparar
         #10; 
+
+        i_dunit_addr=4;
+        #10
+        i_dunit_addr=8;
+        #10
 
         // End simulation
         #500;
